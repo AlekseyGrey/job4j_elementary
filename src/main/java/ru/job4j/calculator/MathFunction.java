d0d910538f6c62967cb29ed2b636520e8dce49db
AlekseyPudovkin <lexgreyroom@gmail.com>
2024-02-04 19:04:35 +0300

    4.2. Результат работы метода [#232622 #52690]

---
+++ src/main/java/ru/job4j/calculator/MathFunction.java
@@ -0,0 +1,14 @@
+package ru.job4j.calculator;
+
+public class MathFunction {
+    public static int func1(int x) {
+        return x;
+    }
+
+    public static void main(String[] args) {
+        int result = MathFunction.func1(100);
+        System.out.println(result);
+
+    }
+
+}
