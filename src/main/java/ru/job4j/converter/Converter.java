51e08b858af4c93902dd7d2f10bf033a2f9af9dc
Aleksey <lexgreyroom@gmail.com>
2024-02-06 14:32:52 +0300

    0. Что такое тестирование [#159431 #52656]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -1,20 +1,44 @@
 package ru.job4j.converter;
 
 public class Converter {
-    public static float rubleToEuro(float value) {
-        float result = value / 70;
+    public static int rubleToEuro(int value) {
+        int result = value / 70;
         return result;
     }
 
-    public static float rubleToDollar(float value) {
-        float result = value / 60;
+    public static int rubleToDollar(int value) {
+        int result = value / 60;
         return result;
     }
 
     public static void main(String[] args) {
-        float euro = Converter.rubleToEuro(140);
+        int euro = Converter.rubleToEuro(140);
         System.out.println("140 rubles are " + euro + " euro.");
-        float dollar = Converter.rubleToDollar(180);
+        int dollar = Converter.rubleToDollar(180);
         System.out.println("180 rubles are " + dollar + " dollars");
+
+        int in1 = 140;
+        int expectedEuro = 2;
+        int outEuro = Converter.rubleToEuro(in1);
+        boolean passed1 = expectedEuro == outEuro;
+        System.out.println("140 rubles are 2. Test result : " + passed1);
+
+        int in2 = 360;
+        int expectedDollar = 6;
+        int outDollar = Converter.rubleToDollar(in2);
+        boolean passed2 = expectedDollar == outDollar;
+        System.out.println("360 rubles are 6. Test result : " + passed2);
+
+        int in3 = 730;
+        int expectedRuble = 53290;
+        int outRuble = Converter.rubleToDollar(in3);
+        boolean passed3 = expectedRuble == outRuble;
+        System.out.println("730 dollars are 53290. Test result : " + passed3);
+
+        int in4 = 140;
+        int expected = 2;
+        int out = Converter.rubleToEuro(in4);
+        boolean passed4 = expected == out;
+        System.out.println("120 euros are 240. Test result : " + passed4);
     }
 }
